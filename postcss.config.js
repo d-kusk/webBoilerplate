31ac5df16069bd672cae487320194fd7099e9df2
Daisuke Konishi <skydai1151@gmail.com>
2017-02-12 02:52:26 +0900

    対応ブラウザの指定を追加

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: [
       require('precss'),
-      require('autoprefixer')
+      require('autoprefixer')({
+        browsers: ['last 2 versions', '> 2%']
+      })
   ]
 }
